f802cd7c4b35875a62793e0887b340d1c03ed332
Syeda Zahra <43184865+Syeda-Zahra@users.noreply.github.com>
2019-06-21 21:25:29 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <nav class="navbar sticky-top navbar-dark bg-dark" role="navigation">
+    <div class="container">
+      <h1> Kalam   <img src="feather.svg" alt="$0"/> </h1>
+      <form class="nv">
+        <a href="home.php">Home</a>
+        <a href="about.php">About Us</a>
+        <a href="logout.php">Logout</a>
+      </form>
+    </div>
+  </nav>
+  <div class="container">
+  </br>
+    <h2 align="center">About Us </h2>
+    <p align="center">This is a simple to-do list web application that allows you to get organised.
+    </br>
+    The design of the application was kept very simple to allow users of all levels
+    </br>
+    to be able to use the application.In addtion to creating a to-do list, the user
+    </br>
+    can also check the weather by entering their city name and clicking the
+    </br>
+    "Get Weather" button.
+    </p>
+  </div>
+</body>
+</html>
